a17158c41c178902da713cc4e02d4712285fa70a
Cielo <macielo.tablate@gmail.com>
2022-01-20 09:52:07 +0100

    Add Day42

--- Day41/day41.js
+++ Day41/day41.js
@@ -1,9 +1,5 @@
 document.querySelector('.closebtn').addEventListener('click', showErrorMsg);
 
-let element = true
-
 function showErrorMsg(){
-    if(element === true){
         document.querySelector('.card').classList.add('active')
-    }
 }
