6584e423a4e90621bd97e3bb12ee2763980652f7
Cielo <macielo.tablate@gmail.com>
2022-01-27 17:16:32 +0100

    Ammend the JS

--- Day44/day44.js
+++ Day44/day44.js
@@ -1,5 +1,16 @@
-const sqcolor = document.querySelectorAll('.sq');
+const frame = document.createElement("div");
+frame.classList.add("frame");
 
+for(i = 1; i <= 20; i++){
+    const square = document.createElement("span");
+    square.classList.add("sq");
+    square.classList.add("sq-" + i);
+    frame.appendChild(square);
+}
+
+document.body.appendChild(frame);
+
+const sqcolor = document.querySelectorAll('.sq');
 for(let i = 0; i < sqcolor.length; i++) {
     let rgb = [];
 
