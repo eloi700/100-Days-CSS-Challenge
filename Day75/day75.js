5aa87416e3ca33c679bd5da89845ae292c2d8afd
Cielo <macielo.tablate@gmail.com>
2022-03-09 08:05:56 +0100

    Change Lines

--- Day75/day75.js
+++ Day75/day75.js
@@ -1,8 +1,12 @@
 const frame = document.createElement('div')
 const circle = document.createElement('div')
+const title = document.createElement('div')
 frame.classList.add('frame')
+title.classList.add('title')
+title.textContent = "DAY 75 - SPINNING DISCS"
 circle.classList.add('circle')
 document.body.appendChild(frame)
+frame.appendChild(title)
 frame.appendChild(circle)
 
 for (x = 1; x <= 6; x++){
